4df18b34ddc78269c4d5172a037b42a5c9a3be85
Avinash Valavoju <avinash@example.com>
2025-12-18 20:50:34 +0530

    debugging

--- backend /services/inventory.py
+++ backend /services/inventory.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import os
 
-
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DATA_DIR = os.path.join(BASE_DIR, "data")
 
@@ -10,9 +9,12 @@ SALES = os.path.join(DATA_DIR, "pharmacy_sales_noisy.json")
 
 
 def get_inventory():
-    purchases = pd.read_json(PURCHASES, lines=True)
+    purchases = pd.read_json(PURCHASES)
+    sales = pd.read_json(SALES)
 
-    sales = pd.read_json(SALES, lines=True)
+    # Normalize drug names (VERY IMPORTANT)
+    purchases["Drug_Name"] = purchases["Drug_Name"].str.lower().str.replace("-", " ")
+    sales["Drug_Name"] = sales["Drug_Name"].str.lower().str.replace("-", " ")
 
     purchased = purchases.groupby("Drug_Name")["Qty_Received"].sum()
     sold = sales.groupby("Drug_Name")["Qty_Sold"].sum()
