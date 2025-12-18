3200d71e4a7e220c447f51e3be572d058cf954fa
Avinash Valavoju <avinash@example.com>
2025-12-18 21:00:45 +0530

    debugging

--- backend /app.py
+++ backend /app.py
@@ -1,7 +1,8 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from services.inventory import get_inventory
 from services.expiry import get_expiry_alerts
 from services.forecast import get_forecast, get_reorder
+from services.chatbot import chatbot_response
 
 app = Flask(__name__)
 
@@ -20,6 +21,12 @@ def forecast():
 @app.route("/reorder")
 def reorder():
     return jsonify(get_reorder())
+
+@app.route("/chatbot", methods=["POST"])
+def chatbot():
+    data = request.get_json()
+    user_message = data.get("message", "")
+    return chatbot_response(user_message)
 
 @app.route("/")
 def home():
